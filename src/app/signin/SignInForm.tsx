ee986a62423f153b1158e0cdb5b19012b497793f
KwonNaRu <kwonnaru@kakao.com>
2024-11-11 14:32:17 +0900

    스타일 수정

--- src/app/signin/SignInForm.tsx
+++ src/app/signin/SignInForm.tsx
@@ -1,7 +1,7 @@
 // components/SignUpForm.tsx
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { loginSchema } from "@/validationSchemas"; // 로그인용 유효성 검사 스키마
@@ -11,6 +11,8 @@ import { signIn } from "@/store/auth/authSlice"; // login action 가져오기
 import styles from "./signin.module.scss";
 import { decodeToken } from "react-jwt";
 import Cookies from "js-cookie";
+import Alert from "@/components/Alert";
+import { useRouter } from "next/navigation";
 
 // 로그인 폼에 입력될 데이터의 타입 정의
 interface LoginFormInputs {
@@ -28,7 +30,11 @@ const SignInForm: React.FC = () => {
         resolver: yupResolver(loginSchema), // yup 스키마로 유효성 검사
     });
 
+    const [errorMessage, setErrorMessage] = useState("");
+    const [showError, setShowError] = useState(false);
+
     const dispatch = useDispatch(); // Redux dispatch 함수
+    const router = useRouter();
 
     // 폼 제출 시 실행될 함수
     const onSubmit = async (data: LoginFormInputs) => {
@@ -50,34 +56,40 @@ const SignInForm: React.FC = () => {
 
             // 로그인 성공 시 Redux 상태 업데이트
             dispatch(signIn({ username, email }));
+            router.push("/");
         } catch (error) {
             console.error(error); // 에러 발생 시 로그
+            setErrorMessage("로그인에 실패했습니다. 다시 시도해 주세요.");
+            setShowError(true);
         }
     };
 
     return (
-        <form className={styles["sign-form"]} onSubmit={handleSubmit(onSubmit)}>
-            <h2>Login</h2>
+        <>
+            {showError && errorMessage && <Alert message={errorMessage} type="error" onClose={() => setShowError(false)} />}
+            <form className={styles["sign-form"]} onSubmit={handleSubmit(onSubmit)}>
+                <h2>Login</h2>
 
-            {/* 이메일 입력 필드 */}
-            <div className={styles["form-group"]}>
-                <label htmlFor="email">Email</label>
-                <input id="email" {...register("email")} className={`${styles.input} ${errors.email ? `${styles["input-error"]}` : ""}`} placeholder="Enter your email" />
-                {errors.email && <p className={styles["error-message"]}>{errors.email.message}</p>}
-            </div>
+                {/* 이메일 입력 필드 */}
+                <div className={styles["form-group"]}>
+                    <label htmlFor="email">Email</label>
+                    <input id="email" {...register("email")} className={`${styles.input} ${errors.email ? `${styles["input-error"]}` : ""}`} placeholder="Enter your email" />
+                    {errors.email && <p className={styles["error-message"]}>{errors.email.message}</p>}
+                </div>
 
-            {/* 비밀번호 입력 필드 */}
-            <div className={styles["form-group"]}>
-                <label htmlFor="password">Password</label>
-                <input id="password" type="password" {...register("password")} className={`${styles.input} ${errors.password ? `${styles["input-error"]}` : ""}`} placeholder="Enter your password" />
-                {errors.password && <p className={styles["error-message"]}>{errors.password.message}</p>}
-            </div>
+                {/* 비밀번호 입력 필드 */}
+                <div className={styles["form-group"]}>
+                    <label htmlFor="password">Password</label>
+                    <input id="password" type="password" {...register("password")} className={`${styles.input} ${errors.password ? `${styles["input-error"]}` : ""}`} placeholder="Enter your password" />
+                    {errors.password && <p className={styles["error-message"]}>{errors.password.message}</p>}
+                </div>
 
-            {/* 제출 버튼 */}
-            <button type="submit" className={styles["btn-submit"]}>
-                Login
-            </button>
-        </form>
+                {/* 제출 버튼 */}
+                <button type="submit" className={styles["btn-submit"]}>
+                    Login
+                </button>
+            </form>
+        </>
     );
 };
 
