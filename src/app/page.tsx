61adc997ed47b54f418808bb3fca9199659b13f8
KwonNaRu <kwonnaru@kakao.com>
2024-11-13 14:21:01 +0900

    랜딩페이지 메인 컨텐츠 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,19 @@
 import Logo from "@/components/Common/Logo";
 import styles from "./page.module.scss";
 import Header from "@/components/Common/Header";
+import CategoryList from "@/components/Category/CategoryList";
 
 export default function Home() {
     return (
         <div className={styles["landing-container"]}>
             <Header />
-            <header className={styles["landing-header"]}>
+            <section className={styles["landing-introduction"]}>
                 <Logo width={250} height={250} />
                 <h1>Welcome to Naru&apos;s blog</h1>
-            </header>
-            <section className={styles["landing-features"]}>
-                <div className={styles["feature"]}>
-                    <h2>Feature 1</h2>
-                    <p>Discover amazing features that will enhance your experience.</p>
-                </div>
-                <div className={styles["feature"]}>
-                    <h2>Feature 2</h2>
-                    <p>Seamless integration with your favorite tools and services.</p>
-                </div>
-                <div className={styles["feature"]}>
-                    <h2>Feature 3</h2>
-                    <p>High performance and reliability with top-notch security.</p>
-                </div>
             </section>
+            <main className={styles["landing-main"]}>
+                <CategoryList />
+            </main>
             <footer className={styles["landing-footer"]}>
                 <p>&copy; 2024 Our Platform. All rights reserved.</p>
             </footer>
