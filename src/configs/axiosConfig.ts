19ab82307796bd1a5f31478ed99a4e6b59a16ded
KwonNaRu <kwonnaru@kakao.com>
2024-11-13 18:31:40 +0900

    axios에서 쿠키 전송 허용

--- src/configs/axiosConfig.ts
+++ src/configs/axiosConfig.ts
@@ -4,7 +4,8 @@ const axiosInstance = axios.create({
     baseURL: 'http://localhost:8080',
     headers: {
         'Content-Type': 'application/json',
-    }
+    },
+    withCredentials: true, // 쿠키를 포함해 요청을 보냄
 });
 
 axiosInstance.interceptors.request.use(
