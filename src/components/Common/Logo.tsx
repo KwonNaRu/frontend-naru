46e8ee3b6395115fbf516ecab4da25cfba16a18c
KwonNaRu <kwonnaru@kakao.com>
2024-11-15 11:24:29 +0900

    로딩 방식 props 추가 및 적용

--- src/components/Common/Logo.tsx
+++ src/components/Common/Logo.tsx
@@ -6,13 +6,15 @@ import styles from "./Logo.module.scss";
 export default function Logo({
     width,
     height,
+    loading = "eager",
 }: Readonly<{
     width: number;
     height: number;
+    loading: "eager" | "lazy";
 }>) {
     return (
         <Link href="/" className={styles.logo}>
-            <Image src="/logo.webp" alt="logo" width={width} height={height} />
+            <Image src="/logo.webp" alt="logo" width={width} height={height} loading={loading} />
         </Link>
     );
 }
