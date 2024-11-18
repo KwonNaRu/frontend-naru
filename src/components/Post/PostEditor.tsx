95c4b46e1b228eed8ddca6d94f27de7c18baa85b
KwonNaRu <kwonnaru@kakao.com>
2024-11-18 21:16:33 +0900

    웹 소켓 통신으로 게시물 저장

--- src/components/Post/PostEditor.tsx
+++ src/components/Post/PostEditor.tsx
@@ -1,14 +1,18 @@
+"use client";
+
 // components/post.tsx
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import styles from "./PostEditor.module.scss";
 import { PostFormInputs } from "@/types/post";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { postSchema } from "@/validationSchemas";
-import axios from "@/configs/axiosConfig";
 import Alert from "../Alert/Alert";
-// import { useAppSelector } from "@/store/hooks";
+import { useAppSelector } from "@/store/hooks";
+import { Client } from "@stomp/stompjs";
+import SockJS from "sockjs-client";
+import Cookies from "js-cookie";
 
 interface PostFormType {
     postFormInputs: PostFormInputs;
@@ -16,10 +20,9 @@ interface PostFormType {
     onClose: () => void;
 }
 
-const PostEditor: React.FC<PostFormType> = ({ author, onClose }) => {
+const PostEditor: React.FC<PostFormType> = ({ postFormInputs, author, onClose }) => {
     const {
         register,
-        handleSubmit,
         formState: { errors },
         reset,
     } = useForm<PostFormInputs>({
@@ -29,27 +32,61 @@ const PostEditor: React.FC<PostFormType> = ({ author, onClose }) => {
     const [errorMessage, setErrorMessage] = useState("");
     const [showError, setShowError] = useState(false);
 
-    // const { user } = useAppSelector((state) => state.auth);
+    const { user } = useAppSelector((state) => state.auth);
 
-    // const handleSave = () => {
-    //     // 저장 로직 추가 (예: API 호출)
-    //     setIsEditing(false);
-    //     // 여기서 editedContent를 서버에 저장하는 로직을 추가
-    // };
+    const isEditable = useMemo(() => user?.role === "OWNER", [user]);
 
-    const onSubmit = async (data: PostFormInputs) => {
+    const client = useRef<Client | null>(null);
+
+    useEffect(() => {
         try {
-            setErrorMessage("");
-            setShowError(false);
-            const response = await axios.post("/categories", data);
-            console.log(response.data); // 서버 응답 로그
-            onClose();
+            // SockJS와 STOMP 클라이언트를 사용해 WebSocket 연결 설정
+            client.current = new Client({
+                webSocketFactory: () => new SockJS("http://localhost:8080/ws"), // 인스턴스 생성 시점 변경
+                reconnectDelay: 5000, // 연결 실패 시 5초 후 재시도
+                heartbeatIncoming: 4000,
+                heartbeatOutgoing: 4000,
+            });
+
+            client.current.connectHeaders = {
+                Authorization: `Bearer ${Cookies.get("NID_AUTH")}`,
+            };
+
+            client.current.onConnect = (frame) => {
+                console.log("Connected: " + frame);
+
+                // /topic/posts 경로에 구독 설정
+                client.current?.subscribe("/topic/posts", (message) => {
+                    console.log("Received message: ", message.body);
+                });
+
+                // 서버에 메시지 보내기 - 목적지 경로는 /app/post
+                client.current?.publish({
+                    destination: "/app/post",
+                    body: JSON.stringify({
+                        postId: postFormInputs.postId,
+                        title: "Updated title",
+                        content: "Updated content",
+                    }),
+                });
+            };
+
+            client.current!.onStompError = (frame) => {
+                console.error("Broker reported error: " + frame.headers["message"]);
+                console.error("Additional details: " + frame.body);
+                setErrorMessage("게시글 수정에 실패했습니다. 다시 시도해 주세요.");
+            };
+
+            client.current?.activate(); // STOMP 클라이언트 활성화
+
+            return () => {
+                // 컴포넌트 언마운트 시 연결 해제
+                client.current?.deactivate();
+            };
         } catch (error) {
-            setErrorMessage("카테고리 생성에 실패했습니다. 다시 시도해 주세요.");
-            setShowError(true);
             console.error(error);
         }
-    };
+    }, []);
 
     useEffect(() => {
         reset({
@@ -58,35 +95,39 @@ const PostEditor: React.FC<PostFormType> = ({ author, onClose }) => {
             content: "",
             category: null,
         });
+
+        return () => {
+            // 컴포넌트 언마운트 시 연결 해제
+            client.current?.deactivate();
+        };
     }, [onClose, reset]);
 
     return (
         <>
             {showError && errorMessage && <Alert message={errorMessage} type="error" onClose={() => setShowError(false)} />}
-            <form onSubmit={handleSubmit(onSubmit)} className={styles["post-edit__form"]}>
+            <form className={styles["post-edit__form"]}>
                 <div>
                     <label htmlFor="title">제목</label>
-                    <input type="text" id="title" {...register("title")} className={styles["post__input"]} />
+                    {isEditable ? <input type="text" id="title" {...register("title")} className={styles["post__input"]} /> : <span>{postFormInputs.title}</span>}
                 </div>
                 {errors.title && <p className={styles["error-message"]}>{errors.title.message}</p>}
                 <div>
                     <label htmlFor="category">카테고리</label>
-                    <input type="text" id="category" {...register("category")} className={styles["post__input"]} />
+                    {isEditable ? <input type="text" id="category" {...register("category")} className={styles["post__input"]} /> : <span>{postFormInputs.category}</span>}
                 </div>
                 {errors.category && <p className={styles["error-message"]}>{errors.category.message}</p>}
                 <div>
                     <label htmlFor="author">작성자</label>
-                    <div id="author" className={styles["post-author"]}>
+                    <span id="author" className={styles["post-author"]}>
                         {author}
-                    </div>
+                    </span>
                 </div>
                 <hr />
                 <label htmlFor="author" className="sr-only">
                     내용
                 </label>
-                <textarea id="content" {...register("content")} className={styles["post__textarea"]} />
+                {isEditable ? <textarea id="content" {...register("content")} className={styles["post__textarea"]} /> : <span>{postFormInputs.content}</span>}
                 {errors.content && <p className={styles["error-message"]}>{errors.content.message}</p>}
-                <button type="submit">생성하기</button>
             </form>
         </>
     );
