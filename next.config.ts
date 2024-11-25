0cae2f3c7a975405cf439c36aebe69284b142714
KwonNaRu <kwonnaru@kakao.com>
2024-11-25 11:12:21 +0900

    output: 'export' - 정적 HTML 파일을 생성하도록 지시하며, npm run build 명령에 적용됩니다. 이 설정은 정적 사이트 생성을 목적으로 하고 있으며, Next.js가 페이지를 HTML 파일로 빌드하여 정적 사이트처럼 호스팅할 수 있도록 하는 데 사용됩니다.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 import path from 'path'
 
 const nextConfig: NextConfig = {
+  output: 'export',
   /* config options here */
   webpack: (config) => {
     // Webpack 설정 커스터마이징 부분
