243c7b408916cfd265af5b42bcf8d75d4c477abd
KwonNaRu <kwonnaru@kakao.com>
2024-11-25 16:06:09 +0900

    빌드 복구

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import path from 'path'
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/frontend-naru' : '', // GitHub Pages에서 사용하는 리포지토리 이름을 지정
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/frontend-naru/' : '',
   webpack: (config) => {
     // Webpack 설정 커스터마이징 부분
     // config.module.rules.push({
